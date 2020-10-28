a99a1008992cc7976ed685486002734bd90d86c8
Kirill Elizarov <kiril.elizarov@gmail.com>
2020-10-29 00:22:33 +0100

    Initial commit

---
+++ OnlineBanking.BL/BankTransactionDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OnlineBanking.BL
+{
+    public class BankTransactionDto
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Change { get; set; }
+        public string Currency { get; set; }
+        public string Summary { get; set; }
+    }
+}
---
+++ OnlineBanking.BL/ITransactionService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OnlineBanking.BL
+{
+    public interface ITransactionService
+    {
+        Task<BankTransactionDto> GetTransactionAsync(int id, CancellationToken cancellationToken = default);
+        Task<IList<BankTransactionDto>> GetTransactionListAsync(CancellationToken cancellationToken = default);
+    }
+}
